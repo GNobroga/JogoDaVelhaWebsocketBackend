c97c902ec877e41a2b0f797db8ede058dc9d7151
Gabiroba <gabrielcardosogirarde@gmail.com>
2024-01-07 15:01:02 -0300

    Adicionando das outras camadas na application

--- JogoVelha.Application/Program.cs
+++ JogoVelha.Application/Program.cs
@@ -1,4 +1,7 @@
 using Asp.Versioning;
+using JogoVelha.Infrastructure;
+using JogoVelha.Service;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +25,12 @@ apiVersioningBuilder.AddApiExplorer(options => {
     options.GroupNameFormat = "'v'VVV";
     options.SubstituteApiVersionInUrl = true;
 });
+
+builder.Services.AddDbContext<SqliteDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
+
+builder.Services.AddSqliteConfiguration(builder.Configuration);
+builder.Services.AddServiceLayerServices(builder.Configuration);
 
 var app = builder.Build();
 
